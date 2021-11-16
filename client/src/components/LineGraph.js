32dc801abc5d69d642a198b5fe9c97a329bf9c82
EC2 Default User <ec2-user@ip-172-30-3-242.ap-northeast-2.compute.internal>
2021-11-16 04:49:24 +0000

    front request url modified & docker file added

--- client/src/components/LineGraph.js
+++ client/src/components/LineGraph.js
@@ -20,7 +20,7 @@ function LineGraph() {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        axios.get('http://localhost:3100/api/alltime/'+targetCountry.toLowerCase())
+        axios.get('http://k8s-default-backendi-6566bc7d31-326551117.ap-northeast-2.elb.amazonaws.com/api/alltime/'+targetCountry.toLowerCase())
         .then(res => {
             console.log(res.data)
             const temp = res.data
