e3b864cc46dd68a05541545a1966add5ca726c75
JeongCheolLee <85550653+JeongCheolLee@users.noreply.github.com>
2021-11-22 15:10:08 +0900

    check ci/cd

--- client/src/components/Graph.js
+++ client/src/components/Graph.js
@@ -10,7 +10,7 @@ function Graph()
         <div id = 'graph' style = {{paddingBottom : '13%', paddingTop : '3%', textAlign : 'center'}}>
             <h1 style = {{color : 'yellow'}}> Chart </h1>
             <p style = {{color : 'yellow'}}>
-                차트를 확인하고 싶으신 국가를 선택해 주세요
+                차트를 확인하고 싶으신 국가를 선택해 주세요.
             </p>
 
             <div style = {{display : 'flex',justifyContent : 'center'}}> 
